9b229ed1c050abfa41e322aaa7672bee048c98a4
Philip Sørensen <git@ppsa.dk>
2023-09-20 13:32:19 +0200

    better use of placeholder

--- src/resources/views/components/password.blade.php
+++ src/resources/views/components/password.blade.php
@@ -6,7 +6,7 @@
 
 <div class="{{ $col }} mb-3">
 	<x-formcomponents::label :id="$id" :name="$name" />
-	<input class="form-control @error($id) is-invalid @enderror" id="{{ $id }}" name="{{ $id }}" placeholder="{{ $name }}" type="password" value="{{ old($id) }}" {{ $attributes }}>
+	<input class="form-control @error($id) is-invalid @enderror" id="{{ $id }}" name="{{ $id }}" type="password" value="{{ old($id) }}" {{ $attributes(['placeholder' => $name]) }}>
 
 	<x-formcomponents::error :name="$id" />
 </div>
