9853dafb52eb6e4bf94ff01d1f942f47191263e8
Philip Sørensen <git@ppsa.dk>
2023-09-20 17:52:50 +0200

    improved button

--- src/resources/views/components/button.blade.php
+++ src/resources/views/components/button.blade.php
@@ -1,7 +1,8 @@
 @props([
+	'class' => 'btn-primary',
 	'col' => 'col-12'
 ])
 
 <div class="{{ $col }}">
-	<button class="btn btn-primary form-control">{{ $slot }}</button>
+	<button class="btn {{ $class }} form-control">{{ $slot }}</button>
 </div>
