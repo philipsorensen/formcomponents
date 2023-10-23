a4d3b4f22581336860fa75db2f4114b536151b59
Philip Sørensen <github@ppsa.dk>
2023-10-23 22:59:12 +0200

    fix

--- src/resources/views/components/text.blade.php
+++ src/resources/views/components/text.blade.php
@@ -7,7 +7,7 @@
 
 <div class="{{ $col }} mb-3">
 	<x-formcomponents::label :id="$id" :name="$name" />
-	<textarea class="form-control @error($id) is-invalid @enderror" id="{{ $id }}" name="{{ $id }}" {{ attributes(['rows' => 12]) }}>{{ $value }}</textarea>
+	<textarea class="form-control @error($id) is-invalid @enderror" id="{{ $id }}" name="{{ $id }}" {{ $attributes(['rows' => 12]) }}>{{ $value }}</textarea>
 	
 	<x-formcomponents::error :name="$id" />
 </div>
