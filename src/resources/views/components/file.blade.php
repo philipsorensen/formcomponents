efc94fb1684b8879fa564c08f65d54d295f1b987
Philip Sørensen <github@ppsa.dk>
2023-11-11 12:20:25 +0100

    added a file component

--- src/resources/views/components/file.blade.php
+++ src/resources/views/components/file.blade.php
@@ -1,12 +1,13 @@
 @props([
+	'accept' => 'text/plain',
 	'col' => 'col-12',
-	'id' => 'image',
+	'id' => 'file',
 	'name' => null,
 ])
 
 <div class="{{ $col }} mb-3">
 	@if ($name) <x-formcomponents::label :id="$id" :name="$name" /> @endif
-	<input accept="image/*" class="form-control @error($id) is-invalid @enderror" id="{{ $id }}" name="{{ $id }}" type="file" {{ $attributes }}>
+	<input accept="{{ $accept }}" class="form-control @error($id) is-invalid @enderror" id="{{ $id }}" name="{{ $id }}" type="file" {{ $attributes }}>
 
 	<x-formcomponents::error :name="$id" />
 </div>
