9c5ad4e40ecedd0e95f4455431146f2b77d84770
Philip Sørensen <github@ppsa.dk>
2023-11-14 21:25:13 +0100

    made select default optional

--- src/resources/views/components/select.blade.php
+++ src/resources/views/components/select.blade.php
@@ -10,7 +10,7 @@
 	@if ($name) <x-formcomponents::label :id="$id" :name="$name" /> @endif
 	@if ($tooltip) <x-formcomponents::tooltip :url="$tooltip" /> @endif
 	<select class="form-select @error($id) is-invalid @enderror" id="{{ $id }}" name="{{ $id }}">
-		<option disabled>{{ $default }}</option>
+		@if ($default) <option disabled>{{ $default }}</option> @endif
 		{{ $slot }}
 	</select>
 
