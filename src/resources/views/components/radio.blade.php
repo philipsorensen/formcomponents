603c7afcddb4fda3236e1f646e708ddff9e8e87f
Philip Sørensen <github@ppsa.dk>
2025-09-11 10:31:07 +0200

    Adds possible required on radio

--- src/resources/views/components/radio.blade.php
+++ src/resources/views/components/radio.blade.php
@@ -4,13 +4,14 @@
 	'idDistinct' => null,
 	'name',
 	'value',
+	'required' => false,
 ])
 
 @php $idDistinct ??= $id; @endphp
 
 <div class="{{ $col }} mb-3">
 	<div class="form-check mt-sm-2 pt-sm-2">
-		<input class="form-check-input @error($id) is-invalid @enderror" id="{{ $idDistinct }}" name="{{ $id }}" type="radio" value="{{ $value }}">
+		<input class="form-check-input @error($id) is-invalid @enderror" id="{{ $idDistinct }}" name="{{ $id }}" @if($required) required @endif type="radio" value="{{ $value }}">
 		<x-formcomponents::label :id="$idDistinct" :name="$name" class="form-check-label" />
 	</div>
 
