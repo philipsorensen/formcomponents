c97e86e7af4e16dbc7d7dfca2da5ae653f5e10aa
Philip Sørensen <github@ppsa.dk>
2026-01-20 07:46:47 +0100

    Fixing error.

--- src/resources/views/components/checkbox.blade.php
+++ src/resources/views/components/checkbox.blade.php
@@ -9,7 +9,7 @@
 	<div class="form-check mt-sm-2 pt-sm-2">
 		<input type="hidden" name="{{ $id }}" value="0" />
 		<input class="form-check-input @error($id) {{ config('formcomponents.is-invalid-class') }} @enderror" id="{{ $id }}" name="{{ $id }}" type="checkbox" value="1" @if ($checked == 1) checked @endif />
-		<x-formcomponents::label :id="$id" :name="$name" :class="{{ config('formcomponents.label.class') }}" />
+		<x-formcomponents::label :id="$id" :name="$name" :class="config('formcomponents.label.class')" />
 	</div>
 
 	<x-formcomponents::error :name="$id" />
